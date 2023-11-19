fb1d927ea98a9dd6684737fa8507d17b97169dab
Miraziz Khidoyatov <miraziz_khidoyatov@epam.com>
2023-11-20 00:13:16 +0500

    add exceptions

---
+++ Module02-sql/TicketingSystem.Domain/Exceptions/Payment/InvalidCardException.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace TicketingSystem.Domain.Exceptions.Payment
+{
+    [Serializable]
+    public class InvalidCardException : PaymentException
+    {
+        public InvalidCardException() { }
+        public InvalidCardException(string message) : base(message) { }
+        public InvalidCardException(string message, Exception innerException) : base(message, innerException) { }
+        protected InvalidCardException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ Module02-sql/TicketingSystem.Domain/Exceptions/Ticket/TicketAlreadySoldException.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace TicketingSystem.Domain.Exceptions.Ticket
+{
+    [Serializable]
+    public class TicketAlreadySoldException : TicketException
+    {
+        public TicketAlreadySoldException() { }
+        public TicketAlreadySoldException(string message) : base(message) { }
+        public TicketAlreadySoldException(string message, Exception inner) : base(message, inner) { }
+        protected TicketAlreadySoldException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
