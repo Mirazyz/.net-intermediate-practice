fa9e4c8c808746c78feaf6b93a5b974ec939e59d
Miraziz Khidoyatov <miraziz_khidoyatov@epam.com>
2024-01-28 22:19:38 +0500

    add notification messaging using rabbitmq

---
+++ Module02-sql/TicketingSystem.Domain/Interfaces/Services/IMessageProducer.cs
@@ -0,0 +1,7 @@
+﻿namespace TicketingSystem.Domain.Interfaces.Services
+{
+    public interface IMessageProducer
+    {
+        void SendMessage<T>(T message);
+    }
+}
---
+++ Module02-sql/TicketingSystem.Services/MessageProducer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+using TicketingSystem.Domain.Interfaces.Services;
+
+namespace TicketingSystem.Services
+{
+    public class MessageProducer : IMessageProducer
+    {
+        public void SendMessage<T>(T message)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("email_notification");
+
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            channel.BasicPublish(exchange: "", routingKey: "email_notifications", body: body);
+        }
+    }
+}
